b4e366fa7ddf39bf839cd890b95e4720874996f3
jmbeach <jaredbeachdesign@gmail.com>
2018-04-26 09:47:06 -0500

    deploy cmder files

--- scripts/dot files/deploy.ps1
+++ scripts/dot files/deploy.ps1
@@ -1,3 +1,7 @@
 # deploy windows config
 $root = "d:\code\github\jmbeach"
 $win = "$root\windows-config"
+$cmder = "~\scoop\apps\cmder\current\vendor"
+
+Copy-Item "$win\custom-scripts" ~\custom-scripts -Recurse -Force
+Copy-Item "$win\profile.ps1" $cmder -Force 
