417bff8ffffbc16f843c4186146c1b5fcffe5ca9
jmbeach <jaredbeachdesign@gmail.com>
2018-04-27 13:22:52 -0500

    don't offer to restart

--- scripts/iis/enable-iis.ps1
+++ scripts/iis/enable-iis.ps1
@@ -1,24 +1,26 @@
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServer
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-CommonHttpFeatures
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpErrors
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpRedirect
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-ApplicationDevelopment
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-NetFxExtensibility45
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-HealthAndDiagnostics
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpLogging
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-LoggingLibraries
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-RequestMonitor
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpTracing
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-Security
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-RequestFiltering
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-Performance
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerManagementTools
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-IIS6ManagementCompatibility
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-Metabase
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-ManagementConsole
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-BasicAuthentication
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-WindowsAuthentication
+mkdir .\logs\iis
+$logPath = ".\logs\iis\iis-enable.log"
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole -NoRestart -LogPath $logPath
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServer -NoRestart -LogPath $logPath
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-CommonHttpFeatures -NoRestart -LogPath $logPath
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpErrors -NoRestart -LogPath $logPath
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpRedirect -NoRestart -LogPath $logPath
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ApplicationDevelopment -NoRestart -LogPath $logPath
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-NetFxExtensibility45 -NoRestart -LogPath $logPath
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HealthAndDiagnostics -NoRestart -LogPath $logPath
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpLogging -NoRestart -LogPath $logPath
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-LoggingLibraries -NoRestart -LogPath $logPath
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-RequestMonitor -NoRestart -LogPath $logPath
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpTracing -NoRestart -LogPath $logPath
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-Security -NoRestart -LogPath $logPath
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-RequestFiltering -NoRestart -LogPath $logPath
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-Performance -NoRestart -LogPath $logPath
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerManagementTools -NoRestart -LogPath $logPath
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-IIS6ManagementCompatibility -NoRestart -LogPath $logPath
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-Metabase -NoRestart -LogPath $logPath
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ManagementConsole -NoRestart -LogPath $logPath
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-BasicAuthentication -NoRestart
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WindowsAuthentication -NoRestart
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-StaticContent
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-DefaultDocument
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebSockets
