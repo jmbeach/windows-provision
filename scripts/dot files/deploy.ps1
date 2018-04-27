d982f030c58a4aad34d3bac55e597ee94bd3a67f
jmbeach <jaredbeachdesign@gmail.com>
2018-04-27 08:00:23 -0500

    better vim config, starting shims

--- scripts/dot files/deploy.ps1
+++ scripts/dot files/deploy.ps1
@@ -2,16 +2,18 @@
 $root = "d:\code\github\jmbeach"
 $win = "$root\windows-config"
 $cmder = "~\scoop\apps\cmder\current\vendor"
+$cmderConfig = "~\scoop\apps\cmder\current\config"
 
 Copy-Item "$win\custom-scripts" ~\custom-scripts -Recurse -Force
 Copy-Item "$win\profile.ps1" $cmder -Force 
+Copy-Item "$win\ConEmu.xml" $cmderConfig -Force 
 
 # deploy vim config
 $vimfiles = "$root\vim-config"
 Copy-Item "$vimfiles\_vimrc" ~\_vimrc -Force
 
 # install vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~\vimfiles\bundle\Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME\vimfiles\bundle\Vundle.vim
 
 # install vundle Plugins
-vim +PluginInstall +qall
+start 'C:\Program Files (x86)\vim\vim80\vim.exe' +PluginInstall +qall
---
+++ scripts/shims/make-shims.ps1
@@ -0,0 +1,7 @@
+# chrome
+# iisexpress
+# msbuild
+# procexp
+# rapidee
+# ssms
+# visual studio
