ae06c68a754c531ba1eb7d9818f85dc35d3e10b0
jmbeach <jaredbeachdesign@gmail.com>
2019-08-16 12:46:00 -0700

    vim config deploy fixes

--- scripts/dot files/deploy.ps1
+++ scripts/dot files/deploy.ps1
@@ -1,22 +1,11 @@
-# deploy windows config
-$root = "d:\code\github\jmbeach"
-$win = "$root\windows-config"
-$cmder = "~\scoop\apps\cmder\current\vendor"
-$cmderConfig = "~\scoop\apps\cmder\current\config"
-
-Copy-Item "$win\custom-scripts" ~\custom-scripts -Recurse -Force
-Copy-Item "$win\profile.ps1" $cmder -Force 
-Copy-Item "$win\ConEmu.xml" $cmderConfig -Force 
-
 # deploy vim config
-$vimfiles = "$root\vim-config"
-Copy-Item "$vimfiles\_vimrc" ~\_vimrc -Force
+Copy-Item "$home\_vimrc" ~\_vimrc -Force
 
 # install vundle
-git clone https://github.com/VundleVim/Vundle.vim.git $HOME\vimfiles\bundle\Vundle.vim
+# git clone https://github.com/VundleVim/Vundle.vim.git $HOME\vimfiles\bundle\Vundle.vim
 
 # install vundle Plugins
-& 'C:\Program Files (x86)\vim\vim80\gvim.exe' +PluginInstall +qall
+& 'C:\Program Files (x86)\Vim\vim81\gvim.exe' +PluginInstall +qall
 
 # git
 Copy-Item "$root\dot-files\.gitignore" ~\
