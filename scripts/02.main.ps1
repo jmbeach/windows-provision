7c39d9741c3b1c035898de86b42bb1c3d220baca
jmbeach <jaredbeachdesign@gmail.com>
2019-08-16 10:46:25 -0700

    combine steps 2 and three

--- scripts/02.main.ps1
+++ scripts/02.main.ps1
@@ -1,10 +1,37 @@
-# install scoop
-.\scoop\install-scoop.ps1
-
-# install software using scoop
-.\scoop\install-scoop-tools.ps1
-
 # install software using choco
 .\choco\01.install-choco-tools.ps1
 
-Write-Host "Step 2 complete. Restart the machine and then start step 3."
+# setup file structure the way I like it
+& ".\file structure\setup-file-structure.ps1"
+
+# install custom software
+& ".\custom software\1password\get-1password.ps1"
+& ".\custom software\simian\get-simian.ps1"
+& ".\custom software\iis\get-iis.ps1"
+& ".\custom software\pscx\get-pscx.ps1"
+& ".\custom software\regex hero\get-regex-hero.ps1"
+& ".\custom software\search and replace\get-search-and-replace.ps1"
+
+# get my configurations
+& ".\dot files\get-dot-files.ps1"
+& ".\dot files\deploy.ps1" 
+
+# generate shims
+.\shims\make-shims.ps1
+
+# get autohotkey scripts
+.\autohotkey\get-scripts.ps1
+
+# setup cygwin
+.\cygwin\get-apt-cyg.ps1
+
+# install powershell moduels
+&".\powershell modules\install-powershell-modules.ps1"
+
+# install global npm packages
+&".\global npm packages\install-global-npm-packages.ps1"
+
+# download my favorite postman scripts
+.\postman\get-postman-collections.ps1
+
+Write-Host "Provisioning complete."
