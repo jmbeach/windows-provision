cd9d3dbb776eac4df3936c89ab160c9dff6cf483
jmbeach <jaredbeachdesign@gmail.com>
2019-10-26 15:47:48 -0500

    another fix for dot files

--- scripts/dot files/deploy.ps1
+++ scripts/dot files/deploy.ps1
@@ -1,11 +1,13 @@
+$root = "$home\code\github\jmbeach"
+
 # deploy vim config
-Copy-Item "$home\_vimrc" ~\_vimrc -Force
+Copy-Item "$root\vim-config\_vimrc" ~\_vimrc -Force
 
 # install vundle
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME\vimfiles\bundle\Vundle.vim
 
 # install vundle Plugins
-& 'C:\Program Files (x86)\Vim\vim81\gvim.exe' +PluginInstall +qall
+& 'C:\tools\vim\vim81\gvim.exe' +PluginInstall +qall
 
 # git
 Copy-Item "$root\dot-files\.gitignore" ~\
